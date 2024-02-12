1b2d1b29b4277345e1cf7622e0f6bd347f8f7788
Matías <104095940+matiassaitama@users.noreply.github.com>
2024-02-12 11:21:02 -0300

    Add files via upload

---
+++ images/change-this-to-the-article-slug/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import ArticleLayout from "../ArticleLayout.svelte";
+</script>
+
+<ArticleLayout slug="microbiota-can-provide-an-early-indicator-of-several-medical-conditions">
+  <!-- Article content here -->
+  <p>This is a paragraph</p>
+  <br />
+  <ul>
+    <li>
+      This is a list item
+    </li>
+    <li>
+      This is a list item
+    </li>
+  </ul>
+ 
+</ArticleLayout>
