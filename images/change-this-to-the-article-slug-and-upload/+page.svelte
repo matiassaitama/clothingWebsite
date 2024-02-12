40f3cda125e362578efbd3aebd9259d0367ebb53
Matías <104095940+matiassaitama@users.noreply.github.com>
2024-02-12 14:06:49 -0300

    new article

---
+++ images/change-this-to-the-article-slug-and-upload/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import ArticleLayout from "../ArticleLayout.svelte";
+</script>
+
+<ArticleLayout slug="change-this-to-the-article-slug">
+  <!-- Article content here -->
+  <p>This is a paragraph</p>
+  <br />
+  <ul>
+    <li>
+      This is a list item
+    </li>
+    <li>
+      This is a list item
+    </li>
+  </ul>
+ 
+</ArticleLayout>
